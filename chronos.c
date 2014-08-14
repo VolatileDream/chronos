9b294957ee2a37fdcb4e53484da468035340d17c
Gianni Gambetti <gianni@gambetti.ca>
2014-08-14 16:16:36 -0400

    moving main out, allows for linking as a library

--- chronos.c
+++ chronos.c
@@ -2,21 +2,6 @@
 
 #include "headers.h"
 
-int usage( int argc, char** argv ){
-	printf("usage: %s [init|list|get|append] \n\n", argv[0] );
-	
-	printf(" init directory [keysize] - initialize a log in the directory,"
-			" with the desired keysize (default 128 bytes)\n");
-	printf(" list directory           - list all the keys in the log\n");
-	printf(" get directory key        - get the specified key in the log\n");
-	printf(" append directory         - append a value to the log\n");
-	
-	printf("\n");
-
-	printf("%s expects keys to parse to a date\n", argv[0]);
-	return 0;
-}
-
 // returns the fd of the locked directory, or -1 and an error is printed to stderr
 int lock_dir( char* dir, int will_write ){
 
@@ -121,36 +106,5 @@ int open_data( char* dir, int will_write ){
 	// this gets passed through because we have
 	// no fancy checks to do at this time. 
 	return open_file( dir, DATA_FILE, will_write );
-}
-
-static command_t commands[] = {
-	{ .name = "--help", .func = &usage },
-	{ .name = "-h", .func = &usage },
-	{ .name = "?", .func = &usage },
-	{ .name = "init", .func = &init },
-	{ .name = "list", .func = &list },
-	{ .name = "get", .func = &get },
-	{ .name = "submit", .func = &append },
-};
-
-int main(int argc, char** argv){
-
-	if( argc < 3 ){
-		usage( argc, argv );
-		return 2;
-	}
-
-	char* command = argv[1];
-
-	for( int i=0; i < sizeof( commands ) / sizeof( command_t ); i++ ){
-		// 10 because it should be longer than any command name in commands
-		if( strncmp( command, commands[i].name, 10 ) == 0 ){
-			return (*commands[i].func)( argc, argv );
-		}
-	}
-
-	printf("error: unknown command '%s'\n", command );
-	usage( argc, argv );
-	return 2;
 }
 
---
+++ main.c
@@ -0,0 +1,52 @@
+
+#include "chronos.h"
+
+#include "headers.h"
+
+int usage( int argc, char** argv ){
+	printf("usage: %s [init|list|get|append] \n\n", argv[0] );
+	
+	printf(" init directory [keysize] - initialize a log in the directory,"
+			" with the desired keysize (default 128 bytes)\n");
+	printf(" list directory           - list all the keys in the log\n");
+	printf(" get directory key        - get the specified key in the log\n");
+	printf(" append directory         - append a value to the log\n");
+	
+	printf("\n");
+
+	printf("%s expects keys to parse to a date\n", argv[0]);
+	return 0;
+}
+
+// commands for the cli interface
+
+static command_t commands[] = {
+	{ .name = "--help", .func = &usage },
+	{ .name = "-h", .func = &usage },
+	{ .name = "?", .func = &usage },
+	{ .name = "init", .func = &init },
+	{ .name = "list", .func = &list },
+	{ .name = "get", .func = &get },
+	{ .name = "submit", .func = &append },
+};
+
+int main(int argc, char** argv){
+
+	if( argc < 3 ){
+		usage( argc, argv );
+		return 2;
+	}
+
+	char* command = argv[1];
+
+	for( int i=0; i < sizeof( commands ) / sizeof( command_t ); i++ ){
+		// 10 because it should be longer than any command name in commands
+		if( strncmp( command, commands[i].name, 10 ) == 0 ){
+			return (*commands[i].func)( argc, argv );
+		}
+	}
+
+	printf("error: unknown command '%s'\n", command );
+	usage( argc, argv );
+	return 2;
+}
